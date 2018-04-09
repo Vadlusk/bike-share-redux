35a42dfb7d70a35538a0fa2bb0f78b785b7065c3
adam-conway <adam.n.conway@gmail.com>
2018-04-09 16:43:46 -0600

    Added first test for trip index page

--- spec/features/visitor/trips/user_sees_all_trips_spec.rb
+++ spec/features/visitor/trips/user_sees_all_trips_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-describe "Visitor" do
-  scenario "A visitor goes to trip index and sees one trip trips" do
-    # user = User.create!(username: "Adam", password: "password")
-    # budget = user.budgets.create!(name: "Denver")
-    # category1 = budget.categories.create!(title: 'Rent')
-    # category2 = budget.categories.create!(title: 'Food')
-    # category3 = budget.categories.create!(title: 'Car payment')
-    # category4 = budget.categories.create!(title: 'Car insurance')
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+describe 'Visitor' do
+  scenario 'A visitor goes to trip index and sees all trips' do
+    trips = create_list(:trip, 5)
 
-    # visit user_budget_path(user, budget)
+    visit trips_path
 
-    # expect(page).to have_content(category1.title)
-    # expect(page).to have_content(category2.title)
-    # expect(page).to have_content(category3.title)
-    # expect(page).to have_content(category4.title)
+    trips.each do |trip|
+      expect(page).to have_content(trip.duration)
+      expect(page).to have_content(trip.start_date)
+      expect(page).to have_content(trip.start_station_name)
+      expect(page).to have_content(trip.start_station_id)
+      expect(page).to have_content(trip.end_date)
+      expect(page).to have_content(trip.end_station_name)
+      expect(page).to have_content(trip.end_station_id)
+      expect(page).to have_content(trip.bike_id)
+      expect(page).to have_content(trip.subscription_type)
+      expect(page).to have_content(trip.zip_code)
+    end
   end
 end
