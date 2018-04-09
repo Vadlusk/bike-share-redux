384944ea99d5741f340f81e01df41b926838ad38
adam-conway <adam.n.conway@gmail.com>
2018-04-09 16:43:46 -0600

    Added first test for trip index page

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Trip do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with all attributes" do
+      end
+    end
+  end
+end
