d8db14fb3670b5fe3a1f351359b6a3dbd510dfae
agpiermarini <piermarini.andrew@gmail.com>
2018-04-09 16:37:59 -0600

    Add tests for visitor account creation and login

---
+++ spec/user/user_can_create_a_new_account_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'Visitor' do
+  describe 'visits the root path' do
+    it 'see a login link' do
+      visit '/'
+
+      within(".login-button") do
+        expect(page).to have_content("Login")
+      end
+
+      click_on "Login"
+
+      expect(curret_path).to eq("/login")
+    end
+  end
+
+  describe 'can click create a new account link and complete the form' do
+    skip 'and it creates a new account' do
+      visit '/'
+      click_on 'Create Account'
+
+      username = "username"
+      password = "password"
+      email = "email@email.com"
+
+      expect(current_path).to eq("/login")
+
+      fill_in 'user[email]', with: username
+      fill_in 'user[username]', with: username
+      fill_in 'user[password]', with: username
+      fill_in 'user[password_confirmation]', with: username
+      click_on "Create Account"
+
+      expect(current_path).to eq("/dashboard")
+
+      within(".nav") do
+        expect(page).to have_content("Logged in as #{username}")
+      end
+
+      expect(page).to have_css(".logout-button")
+      expect(page).to_not have_css(".login-button")
+    end
+  end
+end
