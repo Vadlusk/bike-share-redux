c0da062e62cd50dd2a2fa3ab974a3de6cc38ab90
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-10 14:18:11 -0600

    Removes unnecessary slug creation in station factory

--- spec/factories/stations.rb
+++ spec/factories/stations.rb
@@ -2,9 +2,9 @@ require 'date'
 
 FactoryBot.define do
   factory :station do
-    sequence(:name) { ['2nd at South Park', 'San Francisco Caltrain 2 (330 Townsend)', 'South Van Ness at Market', 'Mountain View City Hall'] }
+    sequence(:name) { ['2nd at South Park', 'San Francisco Caltrain 2 (330 Townsend)', 'South Van Ness at Market', 'Mountain View City Hall'].sample }
     dock_count { rand(1..30) }
-    sequence(:city) { ['San Jose', 'Mountain View', 'Palo Alto', 'San Francisco'] }
+    sequence(:city) { ['San Jose', 'Mountain View', 'Palo Alto', 'San Francisco'].sample }
     installation_date Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
     latitude { rand(-90..90) }
     longitude { rand(-180..180) }
