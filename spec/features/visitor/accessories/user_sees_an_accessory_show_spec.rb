719a164523fbbb9a1c5910c0be171c0287f1f366
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-10 17:39:29 -0600

    Adds spec test for accessory image appearing as well as functionality to click on Add to Cart

--- spec/features/visitor/accessories/user_sees_an_accessory_show_spec.rb
+++ spec/features/visitor/accessories/user_sees_an_accessory_show_spec.rb
@@ -9,5 +9,16 @@ describe 'visitor' do
     expect(page).to have_content(accessory.title)
     expect(page).to have_content(accessory.description)
     expect(page).to have_content(accessory.price)
+    expect(page).to have_css(".accessory_image")
+  end
+
+  scenario 'a visitor goes to accessory show and clicks add_to_cart' do
+    skip "Waiting on cart to be built. Will need flash message in accessory#show"
+    accessory = create(:accessory)
+
+    visit accessory_path(accessory)
+    click_on 'Add to Cart'
+
+    expect(page).to have_content("You've added #{accessory.title} to your cart")
   end
 end
