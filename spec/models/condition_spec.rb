ea298f58f0d7b433428b05d23ecb22c797d6470f
VadLusk <AdLusk@gmail.com>
2018-04-09 20:42:45 -0600

    Add condition model test, create model file.

---
+++ spec/models/condition_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe Condition do
+  context 'validations' do
+    context 'valid attributes' do
+      it 'is valid with all attributes' do
+        condition = Condition.create!(
+          date: '10/12/2013',
+          max_temp: 60,
+          mean_temp: 50,
+          min_temp: 40,
+          max_dew_point: 40,
+          mean_dew_point: 40,
+          min_dew_point: 30,
+          max_humidity: 75,
+          mean_humidity: 50,
+          min_humidity: 10,
+          max_sea_lvl_pressure: 29.95,
+          mean_sea_lvl_pressure: 30.00,
+          min_sea_lvl_pressure: 29.80,
+          max_visibility: 10,
+          mean_visibility: 5,
+          min_visibility: 0,
+          max_wind_speed: 7,
+          mean_wind_speed: 1,
+          max_gust_speed: 6,
+          precip: 0,
+          cloud_cover: 4,
+          events: 'rain',
+          wind_dir_degrees: 180,
+          zip_code: 80210
+        )
+
+        expect(condition).to be_valid
+      end
+    end
+    context 'invalid attributes' do
+      it 'is invalid without a date' do
+        condition = Condition.create!(
+          date: '',
+          max_temp: 60,
+          mean_temp: 50,
+          min_temp: 40,
+          max_dew_point: 40,
+          mean_dew_point: 40,
+          min_dew_point: 30,
+          max_humidity: 75,
+          mean_humidity: 50,
+          min_humidity: 10,
+          max_sea_lvl_pressure: 29.95,
+          mean_sea_lvl_pressure: 30.00,
+          min_sea_lvl_pressure: 29.80,
+          max_visibility: 10,
+          mean_visibility: 5,
+          min_visibility: 0,
+          max_wind_speed: 7,
+          mean_wind_speed: 1,
+          max_gust_speed: 6,
+          precip: 0,
+          cloud_cover: 4,
+          events: 'rain',
+          wind_dir_degrees: 180,
+          zip_code: 80210
+        )
+
+        expect(condition).to_not be_valid
+      end
+    end
+  end
+end
