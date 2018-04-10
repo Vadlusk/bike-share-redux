3cc08c052ec0f356d82752a718afab7ca9032462
agpiermarini <piermarini.andrew@gmail.com>
2018-04-09 23:10:36 -0600

    Add test to ensure user cannot login with improper credentials

--- spec/features/visitor/visitor_can_login_to_account_spec.rb
+++ spec/features/visitor/visitor_can_login_to_account_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'Visitor' do
-  describe 'as a registered user on root path' do
-    it 'can click login link and login to account with required credentials' do
+  describe 'as a registered' do
+    it 'can login to account with required credentials' do
       visit '/'
       click_on "Login"
 
@@ -22,6 +22,23 @@ describe 'Visitor' do
       end
 
       expect(page).to_not have_content("Login")
+    end
+
+    it 'cannot login to account with incorrect credentials' do
+      visit '/'
+      click_on "Login"
+
+      expect(current_path).to eq("/login")
+
+      user = create(:user)
+
+      fill_in 'username', with: user.username
+      fill_in 'password', with: "bad_password"
+      click_on "Login"
+
+      expect(current_path).to eq("/login")
+
+      expect(page).to have_content("Invalid username and/or password")
     end
   end
 end
