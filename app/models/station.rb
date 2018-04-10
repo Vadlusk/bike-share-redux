e4105b243e4ceb33aebd66fbc65216758bed07fa
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-09 22:41:15 -0600

    Adds lat and long to factory for stations

---
+++ app/models/station.rb
@@ -0,0 +1,8 @@
+class Station < ApplicationRecord
+  validates_presence_of :name,
+                        :dock_count,
+                        :city,
+                        :installation_date,
+                        :latitude,
+                        :longitude
+end
--- spec/factories/stations.rb
+++ spec/factories/stations.rb
@@ -2,9 +2,11 @@ require 'date'
 
 FactoryBot.define do
   factory :station do
-    name { ['2nd at South Park', 'San Francisco Caltrain 2 (330 Townsend)', 'South Van Ness at Market', 'Mountain View City Hall'] }
+    sequence(:name) { ['2nd at South Park', 'San Francisco Caltrain 2 (330 Townsend)', 'South Van Ness at Market', 'Mountain View City Hall'] }
     dock_count { rand(1..30) }
-    city { ['San Jose', 'Mountain View', 'Palo Alto', 'San Francisco'] }
+    sequence(:city) { ['San Jose', 'Mountain View', 'Palo Alto', 'San Francisco'] }
     installation_date Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+    latitude { rand(-90..90) }
+    longitude { rand(-180..180) }
   end
 end
