71df0ad0ec74a9ed50870baf99218483374ae2d4
agpiermarini <piermarini.andrew@gmail.com>
2018-04-09 23:07:02 -0600

    Update schema after running trips migration locally

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 20180410002616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "role", default: 0
-  end
-
   create_table "trips", force: :cascade do |t|
     t.integer "duration"
     t.datetime "start_date"
@@ -38,4 +29,14 @@ ActiveRecord::Schema.define(version: 20180410002616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role", default: 0
+  end
+
 end
