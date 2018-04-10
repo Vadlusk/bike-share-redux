9601fde3f3c71214aeeec301b0c112a09d1e76e9
VadLusk <AdLusk@gmail.com>
2018-04-09 19:35:33 -0600

    Add test for conditions index.

---
+++ spec/features/visitor/conditions/user_sees_all_conditions_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'user sees all conditions' do
+  scenario 'and all their attributes' do
+    conditions = create(:conditions, 10)
+
+    visit conditions_path
+
+    conditions.each do |condition|
+      expect(page).to have_content(condition.date)
+      expect(page).to have_content(condition.max_temp)
+      expect(page).to have_content(condition.mean_temp)
+      expect(page).to have_content(condition.min_temp)
+      expect(page).to have_content(condition.max_dew_point)
+      expect(page).to have_content(condition.mean_dew_point)
+      expect(page).to have_content(condition.min_dew_point)
+      expect(page).to have_content(condition.max_humidity)
+      expect(page).to have_content(condition.mean_humidity)
+      expect(page).to have_content(condition.min_humidity)
+      expect(page).to have_content(condition.max_sea_lvl_pressure)
+      expect(page).to have_content(condition.mean_sea_lvl_pressure)
+      expect(page).to have_content(condition.min_sea_lvl_pressure)
+      expect(page).to have_content(condition.max_visibility)
+      expect(page).to have_content(condition.mean_visibility)
+      expect(page).to have_content(condition.min_visibility)
+      expect(page).to have_content(condition.max_wind_speed)
+      expect(page).to have_content(condition.mean_wind_speed)
+      expect(page).to have_content(condition.min_wind_speed)
+      expect(page).to have_content(condition.precip)
+      expect(page).to have_content(condition.cloud_cover)
+      expect(page).to have_content(condition.events)
+      expect(page).to have_content(condition.wind_dir_degrees)
+      expect(page).to have_content(condition.zip_code)
+    end
+  end
+end
