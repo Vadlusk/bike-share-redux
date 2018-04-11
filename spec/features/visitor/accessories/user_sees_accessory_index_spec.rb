e29ae598bdb4de74c5149b352a46568e4115fc8b
adam-conway <adam.n.conway@gmail.com>
2018-04-10 19:12:39 -0600

    Finished index view for bike-shop

--- spec/features/visitor/accessories/user_sees_accessory_index_spec.rb
+++ spec/features/visitor/accessories/user_sees_accessory_index_spec.rb
@@ -5,12 +5,14 @@ describe 'visitor' do
     accessories = create_list(:accessory, 12)
 
     visit '/bike-shop'
+    save_and_open_page
 
     accessories.each do |accessory|
       expect(page).to have_content(accessory.title)
       expect(page).to have_content(accessory.description)
       expect(page).to have_content(accessory.price)
       expect(page).to have_css(".accessory_image")
+      expect(page).to have_button("Add to Cart")
     end
   end
 end
