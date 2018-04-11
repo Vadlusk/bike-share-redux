f9c939a3071cd9c809eb339e5a20eb3340e5856c
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-11 11:19:57 -0600

    Adds more expectations to ensure test result is accurate

--- spec/features/visitor/stations/user_sees_station_url_and_attributes_spec.rb
+++ spec/features/visitor/stations/user_sees_station_url_and_attributes_spec.rb
@@ -20,5 +20,9 @@ describe 'visitor' do
     visit station_path(station)
 
     expect(current_path).to eq("/stations/#{station.slug}")
+    expect(page).to have_content(station.name)
+    expect(page).to have_content(station.dock_count)
+    expect(page).to have_content(station.city)
+    expect(page).to have_content(station.installation_date)
   end
 end
