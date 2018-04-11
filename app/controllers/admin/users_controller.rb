de76f0d8a36feade689461ec06d886e313efeff1
agpiermarini <piermarini.andrew@gmail.com>
2018-04-10 19:29:23 -0600

    Fix merge conflict during rebase

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,5 @@
-class Admin::UsersController < ApplicationController
-  before_action :require_admin
-
+class Admin::UsersController < Admin::BaseController
   def show
     @user = current_user
-  end
-
-  def require_admin
-    render file: 'public/404' unless current_user.admin?
   end
 end
