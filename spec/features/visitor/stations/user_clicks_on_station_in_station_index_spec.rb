cdebe1462498ce612dbdb8c36a62c57624bb010c
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-11 11:28:14 -0600

    Adds test for clicking station name in stations#index to get to station show page

---
+++ spec/features/visitor/stations/user_clicks_on_station_in_station_index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'visitor' do
+  scenario 'visitor sees all stations on stations index' do
+    stations = create_list(:station, 3)
+
+    visit stations_path
+
+    stations.each do |station|
+      expect(page).to have_content(station.name)
+      expect(page).to have_content(station.dock_count)
+      expect(page).to have_content(station.city)
+      expect(page).to have_content(station.built)
+    end
+
+    click_on "#{stations.first.name}"
+
+    expect(current_path).to eq("/stations/#{stations.first.slug}")
+  end
+end
