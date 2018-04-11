9c4d9a741c89e4353f7f17af7a411f4483c27651
adam-conway <adam.n.conway@gmail.com>
2018-04-10 18:45:14 -0600

    Added test for accessories index

---
+++ spec/features/visitor/accessories/user_sees_accessory_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'visitor' do
+  scenario 'a visitor goes to /bike-shop and sees all accessories' do
+    accessories = create_list(:accessory, 12)
+
+    visit accessories_path
+
+    accessories.each do |accessory|
+      expect(page).to have_content(accessory.title)
+      expect(page).to have_content(accessory.description)
+      expect(page).to have_content(accessory.price)
+      expect(page).to have_css(".accessory_image")
+    end
+  end
+end
