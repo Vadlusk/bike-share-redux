7601a243baf6782b1350e0305ff62fba9fade4cf
agpiermarini <piermarini.andrew@gmail.com>
2018-04-10 18:43:05 -0600

    Change paths in tests to use path helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "New account created successfully!"
+      flash[:success] = "Logged in as #{@user.username}"
       redirect_to dashboard_path
     else
       flash.now[:error] = "Failed to create new account. Please try again."
