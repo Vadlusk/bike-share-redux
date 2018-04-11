eb205e3817d033732560e8ca78a3f5b583ef3560
VadLusk <AdLusk@gmail.com>
2018-04-10 22:18:19 -0600

    Add cart hash to session hash.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,9 @@
 class CartsController < ApplicationController
   def create
     accessory       = Accessory.find(params[:accessory_id])
-    flash[:success] = "You've added 1 #{accessory.title} to your cart"
+    session[:cart] ||= Hash.new(0)
+    session[:cart][accessory.id.to_s] = session[:cart][accessory.id.to_s] + 1
+    flash[:success] = "You've added #{session[:cart][accessory.id.to_s]} #{accessory.title} to your cart"
     redirect_to accessory_path(accessory)
   end
 end
