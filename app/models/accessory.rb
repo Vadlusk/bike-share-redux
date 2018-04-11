f7ff8f78d6019114cd276d766abe809fa35db5bb
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-10 18:14:18 -0600

    Adds method to check status of accessory

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,3 +1,10 @@
 class Accessory < ApplicationRecord
   validates_presence_of :title, :description
+
+  enum status: ['active', 'retired']
+
+  def active?
+    return true if status == 'active'
+    return false if status == 'retired'
+  end
 end
