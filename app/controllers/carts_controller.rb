012b2180dc5a368e2bff11907866915eaa27bd0b
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-11 14:00:06 -0600

    Update carts_controller.rb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,7 @@ class CartsController < ApplicationController
 
   def create
     accessory = Accessory.find(params[:accessory_id])
-    # binding.pry
+
     @cart.add_accessory(accessory.id)
     session[:cart] = @cart.contents
     flash[:success] = "You've added #{pluralize(session[:cart][accessory.id.to_s], accessory.title)} to your cart"
