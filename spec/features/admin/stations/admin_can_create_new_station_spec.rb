859d24526765f162257c791d39fe3b16f0da44f9
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-12 13:31:09 -0600

    Adds test to ensure legitimate lat long field values

--- spec/features/admin/stations/admin_can_create_new_station_spec.rb
+++ spec/features/admin/stations/admin_can_create_new_station_spec.rb
@@ -13,7 +13,7 @@ describe 'user' do
       fill_in 'station[dock_count]', with: 77
       fill_in 'station[city]', with: 'No Way Jose'
       fill_in 'station[installation_date]', with: '01/02/2003'
-      fill_in 'station[lat]', with: 77.777
+      fill_in 'station[lat]', with: -77.777
       fill_in 'station[long]', with: 177.777
       click_on 'Create Station'
 
@@ -23,8 +23,28 @@ describe 'user' do
       expect(page).to have_content(77)
       expect(page).to have_content('No Way Jose')
       expect(page).to have_content(01/02/2003)
-      expect(page).to have_content(77.777)
+      expect(page).to have_content(-77.777)
       expect(page).to have_content(177.777)
+    end
+
+    it 'must use appropriate latitude and longitude ranges' do
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit new_admin_station_path
+
+      fill_in 'station[name]', with: 'Awesome Bike Place'
+      fill_in 'station[dock_count]', with: 77
+      fill_in 'station[city]', with: 'No Way Jose'
+      fill_in 'station[installation_date]', with: '01/02/2003'
+      fill_in 'station[lat]', with: 10000
+      fill_in 'station[long]', with: 200
+      click_on 'Create Station'
+
+      expect(page).to have_button('Create Station')
+      expect(page).to have_content('Station has NOT been created, please make sure you fill in all of the form')
+      expect(page).to_not have_content('You successfully created this station')
     end
   end
 end
