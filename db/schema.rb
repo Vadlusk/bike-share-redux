73863be970439c1fcd82bf1584bd053d03b149a1
agpiermarini <piermarini.andrew@gmail.com>
2018-04-12 21:13:29 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411000052) do
+ActiveRecord::Schema.define(version: 20180413023909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,21 @@ ActiveRecord::Schema.define(version: 20180411000052) do
     t.string "zip_code"
   end
 
+  create_table "order_accessories", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "order_id"
+    t.bigint "accessory_id"
+    t.index ["accessory_id"], name: "index_order_accessories_on_accessory_id"
+    t.index ["order_id"], name: "index_order_accessories_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "stations", force: :cascade do |t|
     t.string "name"
     t.integer "dock_count"
@@ -88,4 +103,7 @@ ActiveRecord::Schema.define(version: 20180411000052) do
     t.integer "role", default: 0
   end
 
+  add_foreign_key "order_accessories", "accessories"
+  add_foreign_key "order_accessories", "orders"
+  add_foreign_key "orders", "users"
 end
