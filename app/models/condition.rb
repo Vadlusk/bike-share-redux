63c9bdf348529c61197cd80ee940322e4a682a07
agpiermarini <piermarini.andrew@gmail.com>
2018-04-14 16:27:20 -0600

    Adds Condition.trips_by_temperature, .return_min, .return_max, .max_trips_by_temperature, .min_trips_by_temperature

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,55 @@
 class Condition < ApplicationRecord
   validates :date, presence: true
+
+  def self.trips_by_temperature(range)
+    joins("JOIN trips ON conditions.date = trips.start_date")
+      .where(max_temperature_f: [range])
+      .group("conditions.date")
+      .count("trips.id")
+  end
+
+
+  def self.min_trips_by_temperature
+    breakdown = Hash.new(0)
+    temperature_ranges.each do |key, value|
+      breakdown[key] = return_min(trips_by_temperature(value))
+    end
+    breakdown
+  end
+
+  def self.max_trips_by_temperature
+    breakdown = Hash.new(0)
+    temperature_ranges.each.each do |key, value|
+      breakdown[key] = return_max(trips_by_temperature(value))
+    end
+    breakdown
+  end
+
+  def self.return_min(hash)
+    hash.min_by{ |key, value| value }
+  end
+
+  def self.return_max(hash)
+    hash.max_by{ |key, value| value }
+  end
+
+  def self.temperature_ranges
+    {"40°F to 49°F"=>40..49,
+     "50°F to 59°F"=>50..59,
+     "60°F to 69°F"=>60..69,
+     "70°F to 79°F"=>70..79,
+     "80°F to 89°F"=>80..89,
+     "90°F to 99°F"=>90..99,
+     "100°F to 110°F"=>100..110}
+  end
 end
+
+
+
+# def self.trips_by_temperature_breakdown
+#   breakdown = Hash.new(0)
+#   temperature_ranges.each do |key, value|
+#     breakdown[key] = trips_by_temperature(value)
+#   end
+#   breakdown
+# end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Condition do
-  context 'validations' do
+  describe 'validations' do
     context 'valid attributes' do
       it 'is valid with all attributes' do
         condition = Condition.create!(
@@ -34,6 +34,7 @@ describe Condition do
         expect(condition).to be_valid
       end
     end
+
     context 'invalid attributes' do
       it 'is invalid without a date' do
         condition = Condition.create(
@@ -67,4 +68,119 @@ describe Condition do
       end
     end
   end
+
+  describe 'class methods' do
+    it '.trips_by_temperature' do
+      date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+      date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+      date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+      create(:condition, date: date_1, max_temperature_f: 75)
+      create(:condition, date: date_2, max_temperature_f: 78)
+      create(:condition, date: date_3, max_temperature_f: 78)
+      create_list(:trip, 3, start_date: date_1)
+      create_list(:trip, 5, start_date: date_2)
+      create_list(:trip, 2, start_date: date_3)
+
+      actual = Condition.trips_by_temperature([70..80])
+      expected =  {date_1=>3, date_2=>5, date_3=>2}
+
+      expect(expected).to eq(actual)
+    end
+
+
+    it '.return_min' do
+      date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+      date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+      date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+
+      actual = Condition.return_min({date_1=>3, date_2=>5, date_3=>2})
+      expected = [date_3, 2]
+
+      expect(actual).to eq(expected)
+    end
+
+    it '.return_max' do
+      date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+      date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+      date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+
+      actual = Condition.return_max({date_1=>3, date_2=>5, date_3=>2})
+      expected = [date_2, 5]
+
+      expect(actual).to eq(expected)
+    end
+
+    it '.max_trips_by_temperature' do
+      date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+      date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+      date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+      create(:condition, date: date_1, max_temperature_f: 75)
+      create(:condition, date: date_2, max_temperature_f: 78)
+      create(:condition, date: date_3, max_temperature_f: 78)
+      create_list(:trip, 3, start_date: date_1)
+      create_list(:trip, 5, start_date: date_2)
+      create_list(:trip, 2, start_date: date_3)
+
+      actual = Condition.max_trips_by_temperature
+      expected = {"40°F to 49°F"=>nil,
+                  "50°F to 59°F"=>nil,
+                  "60°F to 69°F"=>nil,
+                  "70°F to 79°F"=>[date_2, 5],
+                  "80°F to 89°F"=>nil,
+                  "90°F to 99°F"=>nil,
+                  "100°F to 110°F"=>nil
+                }
+
+      expect(actual).to eq(expected)
+    end
+
+    it '.min_trips_by_temperature' do
+      date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+      date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+      date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+      create(:condition, date: date_1, max_temperature_f: 75)
+      create(:condition, date: date_2, max_temperature_f: 78)
+      create(:condition, date: date_3, max_temperature_f: 78)
+      create_list(:trip, 3, start_date: date_1)
+      create_list(:trip, 5, start_date: date_2)
+      create_list(:trip, 2, start_date: date_3)
+
+      actual = Condition.min_trips_by_temperature
+      expected = {"40°F to 49°F"=>nil,
+                  "50°F to 59°F"=>nil,
+                  "60°F to 69°F"=>nil,
+                  "70°F to 79°F"=>[date_3, 2],
+                  "80°F to 89°F"=>nil,
+                  "90°F to 99°F"=>nil,
+                  "100°F to 110°F"=>nil
+                }
+
+      expect(actual).to eq(expected)
+    end
+  end
 end
+
+
+# skip '.trips_by_temperature_breakdown' do
+#   date_1 = Date.strptime('8/29/2013 9:08', '%m/%d/%Y %k:%M')
+#   date_2 = Date.strptime('8/30/2013 9:08', '%m/%d/%Y %k:%M')
+#   date_3 = Date.strptime('8/31/2013 9:08', '%m/%d/%Y %k:%M')
+#   create(:condition, date: date_1, max_temperature_f: 75)
+#   create(:condition, date: date_2, max_temperature_f: 78)
+#   create(:condition, date: date_3, max_temperature_f: 78)
+#   create_list(:trip, 3, start_date: date_1)
+#   create_list(:trip, 5, start_date: date_2)
+#   create_list(:trip, 2, start_date: date_3)
+#
+#   actual = Condition.trips_by_temperature_breakdown
+#   expected = {"40°F to 49°F"=>{},
+#               "50°F to 59°F"=>{},
+#               "60°F to 69°F"=>{},
+#               "70°F to 79°F"=>{date_1=>3, date_2=>5, date_3=>2},
+#               "80°F to 89°F"=>{},
+#               "90°F to 99°F"=>{},
+#               "100°F to 110°F"=>{}
+#             }
+#
+#   expect(actual).to eq(expected)
+# end
