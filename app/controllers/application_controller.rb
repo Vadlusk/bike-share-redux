1550e7cc6dabb5e7e02074e11e41b2794f7d4973
VadLusk <AdLusk@gmail.com>
2018-04-13 22:43:34 -0600

    Refactor with current_admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :current_admin?
   before_action :set_cart
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
   end
 
   def set_cart
