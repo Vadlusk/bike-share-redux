65848ac71ff375398badca9671697e8c31d92e7c
VadLusk <AdLusk@gmail.com>
2018-04-14 12:54:21 -0600

    Add order show test.

--- spec/features/user/dashboard_displays_profile_info_spec.rb
+++ spec/features/user/dashboard_displays_profile_info_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
-describe 'Visitor' do
+describe 'User' do
   describe 'as a registered default user' do
     it 'sees profile information on dashboard page' do
       user = create(:user)
-      visit root_path
-      click_on "Login"
 
-      fill_in 'username', with: user.username
-      fill_in 'password', with: user.password
-      click_on "Login"
+      visit dashboard_path
 
       expect(page).to have_content(user.username)
       expect(page).to have_content(user.email)
+    end
+    it 'sees orders' do
+      user   = create(:user)
+      orders = create_list(:order, 10)
+
+      visit dashboard_path
+
+      orders.each do |order|
+        expect(page).to have_content()
+      end
     end
   end
 end
