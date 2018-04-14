9c879b1a0f4276dd74ed14b7391bf064fe8f01ab
VadLusk <AdLusk@gmail.com>
2018-04-13 23:02:09 -0600

    Add buttons to show.

--- spec/features/admin/trips/admin_sees_one_trip_spec.rb
+++ spec/features/admin/trips/admin_sees_one_trip_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe 'Admin' do
-  context 'visits the trips index' do
+  context 'visits a trip show' do
     scenario 'and it has links to edit and delete trips' do
       admin = create(:admin)
       trip  = create(:trip)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit trips_path
+      visit trip_path(trip)
 
       expect(page).to have_link('Delete')
       expect(page).to have_link('Edit')
