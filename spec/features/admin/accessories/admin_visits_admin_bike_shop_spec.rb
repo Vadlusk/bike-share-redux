b23ac33e20905a6dd5d0eaeb608ca60bca061537
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-16 15:33:55 -0600

    Adds test for admin bike shop showing retired items

--- spec/features/admin/accessories/admin_visits_admin_bike_shop_spec.rb
+++ spec/features/admin/accessories/admin_visits_admin_bike_shop_spec.rb
@@ -14,7 +14,23 @@ describe 'admin' do
       expect(current_path).to eq('/admin/bike-shop')
     end
     it 'sees a table with all accessories' do
+      create_list(:accessory, 20)
+      create(:accessory, title: 'Some junk', status: 1)
+      create(:accessory, title: 'Other junk', status: 1)
+      create(:accessory, title: 'Mostly junk', status: 1)
+      create(:accessory, title: 'Definitely junk', status: 1)
+      create(:accessory, title: 'Not quite junk', status: 1)
+      admin = create(:admin)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_path
+
+      expect(page).to have_content('Some junk')
+      expect(page).to have_content('Other junk')
+      expect(page).to have_content('Mostly junk')
+      expect(page).to have_content('Definitely junk')
+      expect(page).to have_content('Not quite junk')
     end
   end
 end
