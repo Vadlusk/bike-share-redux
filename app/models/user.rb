244c928fa5749860afa316cf9c884dae66ac1cb7
adam-conway <adam.n.conway@gmail.com>
2018-04-16 11:59:54 -0600

    Added dependent destroy to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email,    presence: true, uniqueness: true
-  has_many  :orders
+  has_many  :orders, dependent: :destroy
   has_secure_password
 
   enum role: [ "default", "admin" ]
