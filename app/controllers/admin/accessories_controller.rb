eb3bba9859870d0da6d06dc4f62e508e130490ee
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-16 20:22:21 -0600

    Adds create, new, edit and update methods to admin accessories controller

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -3,9 +3,40 @@ class Admin::AccessoriesController < Admin::BaseController
     @accessories = Accessory.all
   end
 
-  # private
-  #
-  # def accessories_params
-  #   params.require(:accessories).permit()
-  # end
+  def new
+    @accessory = Accessory.new
+  end
+
+  def create
+    @accessory = Accessory.new(accessory_params)
+    if @accessory.save
+      flash.notice = 'You successfully created this accessory'
+      redirect_to admin_bike_shop_path
+    else
+      flash.notice = 'Accessory has NOT been created, please make sure you fill in all of the form'
+      render :new
+    end
+  end
+
+  def edit
+    @accessory = Accessory.find(params[:id])
+  end
+
+  def update
+    @accessory = Accessory.find(params[:id])
+    @accessory.update(accessory_params)
+    if @accessory.save
+      flash.notice = "Accessory has been updated."
+      redirect_to admin_bike_shop_path
+    else
+      flash.notice = "Accessory has NOT been updated."
+      render :edit
+    end
+  end
+
+  private
+
+  def accessory_params
+    params.require(:accessory).permit(:title, :description, :price, :image_url, :status)
+  end
 end
