5625cda02d7d27639cdb4f7e0a368e267409d2f2
VadLusk <AdLusk@gmail.com>
2018-04-16 18:24:13 -0600

    Add model tests.

--- spec/features/user/stations/user_sees_one_station_spec.rb
+++ spec/features/user/stations/user_sees_one_station_spec.rb
@@ -7,8 +7,8 @@ describe 'User' do
     @station = create(:station)
     trips_1  = create_list(:trip, 18, start_station_id: @station.id, end_station_name: 'Good', start_date:'1/3/1999', zip_code: '80210', bike_id: 9)
     trips_2  = create_list(:trip, 2, start_station_id: @station.id, end_station_name: 'Bad')
-    trips_3  = create_list(:trip, 17, end_station_id: @station.id, end_station_name: 'Good')
-    trips_4  = create_list(:trip, 13, end_station_id: @station.id, end_station_name: 'Bad')
+    trips_3  = create_list(:trip, 17, end_station_id: @station.id, start_station_name: 'Good')
+    trips_4  = create_list(:trip, 13, end_station_id: @station.id, start_station_name: 'Bad')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit station_path(@station)
--- spec/models/stations_spec.rb
+++ spec/models/stations_spec.rb
@@ -118,45 +118,105 @@ describe Station do
         expect(station.to_param).to eq(station.slug)
       end
     end
+    describe '#num_rides_started' do
+      it 'counts the number of rides started at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, start_station_id: station.id)
+        trips_2 = create_list(:trip, 2, start_station_id: station.id)
+
+        expect(station.num_rides_started).to eq(20)
+      end
+    end
+    describe '#num_rides_ended' do
+      it 'counts the number of rides ended at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, end_station_id: station.id)
+        trips_2 = create_list(:trip, 2, end_station_id: station.id)
+
+        expect(station.num_rides_ended).to eq(20)
+      end
+    end
+    describe '#frequent_destination' do
+      it 'shows the most frequent destination station for rides that began at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, start_station_id: station.id, end_station_name: 'Good')
+        trips_2 = create_list(:trip, 2, start_station_id: station.id, end_station_name: 'Bad')
+
+        expect(station.frequent_destination).to eq('Good')
+      end
+    end
+    describe '#frequent_origination' do
+      it 'shows the most frequent origination station for rides that ended at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, end_station_id: station.id, start_station_name: 'Good')
+        trips_2 = create_list(:trip, 2, end_station_id: station.id, start_station_name: 'Bad')
+
+        expect(station.frequent_origination).to eq('Good')
+      end
+    end
+    describe '#busiest_date' do
+      it 'shows the date with the highest number of trips started at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, start_station_id: station.id, start_date: '1/1/2009')
+        trips_2 = create_list(:trip, 2, start_station_id: station.id, start_station_name: '1/2/2008')
+
+        expect(station.busiest_date).to eq('1/1/2009')
+      end
+    end
+    describe '#busiest_zip_code' do
+      it 'shows the most frequent zip code for users starting trips at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, start_station_id: station.id, zip_code: '12345')
+        trips_2 = create_list(:trip, 2, start_station_id: station.id, zip_code: '11111')
+
+        expect(station.busiest_zip_code).to eq('12345')
+      end
+    end
+    describe '#busiest_bike' do
+      it 'shows the bike ID most frequently starting a trip at this station' do
+        station = create(:station)
+        trips_1 = create_list(:trip, 18, start_station_id: station.id, bike_id: 5)
+        trips_2 = create_list(:trip, 2, start_station_id: station.id, bike_id: 1)
+
+        expect(station.busiest_bike).to eq(5)
+      end
+    end
+  end
+  describe 'class methods' do
     describe '#total_stations' do
       it 'returns the count of stations' do
         create_list(:station, 12)
-        stations = Station.all
 
-        expect(stations.total_stations).to eq(12)
+        expect(Station.total_stations).to eq(12)
       end
     end
     describe '#average_bikes' do
       it 'returns the average bikes per station' do
         create_list(:station, 12)
-        stations = Station.all
 
-        expect(stations.average_bikes).to eq((Station.average(:dock_count)).round(2))
+        expect(Station.average_bikes).to eq((Station.average(:dock_count)).round(2))
       end
     end
     describe '#most_bikes' do
       it 'returns the most bikes at any station' do
         create_list(:station, 12)
-        stations = Station.all
 
-        expect(stations.most_bikes).to eq(Station.maximum(:dock_count))
+        expect(Station.most_bikes).to eq(Station.maximum(:dock_count))
       end
     end
     describe '#fewest_bikes' do
       it 'returns the fewest bikes at any station' do
         create_list(:station, 12)
-        stations = Station.all
 
-        expect(stations.fewest_bikes).to eq(Station.minimum(:dock_count))
+        expect(Station.fewest_bikes).to eq(Station.minimum(:dock_count))
       end
     end
     describe '#most_station' do
       it 'returns the  station with the most bikes' do
         create_list(:station, 12)
         station = create(:station, dock_count: 31)
-        stations = Station.all
 
-        expect(stations.most_station).to eq([station])
+        expect(Station.most_station).to eq([station])
       end
     end
     describe '#most_station' do
@@ -164,18 +224,16 @@ describe Station do
         create_list(:station, 12)
         station1 = create(:station, name: 'a station', dock_count: 31)
         station2 = create(:station, name: 'b station', dock_count: 31)
-        stations = Station.all
 
-        expect(stations.most_station).to eq([station1, station2])
+        expect(Station.most_station).to eq([station1, station2])
       end
     end
     describe '#fewest_station' do
       it 'returns the  station with the fewest bikes' do
         create_list(:station, 12)
         station = create(:station, dock_count: 0)
-        stations = Station.all
 
-        expect(stations.fewest_station).to eq([station])
+        expect(Station.fewest_station).to eq([station])
       end
     end
     describe '#fewest_station' do
@@ -183,27 +241,24 @@ describe Station do
         create_list(:station, 12)
         station1 = create(:station, name: 'a station', dock_count: 0)
         station2 = create(:station, name: 'b station', dock_count: 0)
-        stations = Station.all
 
-        expect(stations.fewest_station).to eq([station1, station2])
+        expect(Station.fewest_station).to eq([station1, station2])
       end
     end
     describe '#newest_station' do
       it 'returns the station installed most recently' do
         create_list(:station, 12)
         station = create(:station, installation_date: '2020-01-30')
-        stations = Station.all
 
-        expect(stations.newest_station).to eq(station)
+        expect(Station.newest_station).to eq(station)
       end
     end
     describe '#oldest_station' do
       it 'returns the station installed least recently' do
         create_list(:station, 12)
         station = create(:station, installation_date: '1920-01-30')
-        stations = Station.all
 
-        expect(stations.oldest_station).to eq(station)
+        expect(Station.oldest_station).to eq(station)
       end
     end
   end
