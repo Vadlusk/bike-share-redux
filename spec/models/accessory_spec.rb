7ebd2337b9b3a668a7c9a2cdb49251aa95403ed7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-16 20:34:48 -0600

    deletes unnecessary tests from model spec

--- spec/models/accessory_spec.rb
+++ spec/models/accessory_spec.rb
@@ -30,35 +30,5 @@ describe Accessory do
         expect(accessory.active?).to eq(false)
       end
     end
-    context '#activate' do
-      it 'changes the status of an inactive item to active' do
-        accessory = Accessory.create(title: 'Retired Item',
-                                     description: 'An item which can no longer be purchased',
-                                     price: 50.50,
-                                     image_url: "http://www.iwantthatsign.com/WebRoot/BT2/Shops/BT4029/4B92/6F4B/DA52/7B6F/97F3/0A0A/33E8/E557/retired-and-lovin-it-2011.jpg",
-                                     status: 1)
-
-        expect(accessory.active?).to eq(false)
-
-        accessory.activate
-
-        expect(accessory.active?).to eq(true)
-      end
-    end
-    context '#deactivate' do
-      it 'changes the status of an active item to inactive' do
-        accessory = Accessory.create(title: 'Retired Item',
-                                     description: 'An item which can no longer be purchased',
-                                     price: 50.50,
-                                     image_url: "http://www.iwantthatsign.com/WebRoot/BT2/Shops/BT4029/4B92/6F4B/DA52/7B6F/97F3/0A0A/33E8/E557/retired-and-lovin-it-2011.jpg",
-                                     status: 0)
-
-        expect(accessory.active?).to eq(true)
-
-        accessory.deactivate
-
-        expect(accessory.active?).to eq(false)
-      end
-    end
   end
 end
