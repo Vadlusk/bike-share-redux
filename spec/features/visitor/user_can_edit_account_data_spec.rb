0a26e6a848571157a3a3dbecf793a6f347dd7fdd
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-04-17 16:27:22 -0600

    Adds spec test for admin and user not changning another users account info

--- spec/features/visitor/user_can_edit_account_data_spec.rb
+++ spec/features/visitor/user_can_edit_account_data_spec.rb
@@ -25,5 +25,29 @@ describe 'user' do
 
       expect(page).to have_content('This user has been updated')
     end
+    it 'cannot update anothers info' do
+      user1 = create(:user, id: 1, username: 'Bobby', password: 'password', email: 'bob@email.com')
+      user2 = create(:user, id: 2, username: 'Dougy', password: 'password', email: 'doug@email.com')
+
+      visit root_path
+
+      click_on "Login"
+
+      fill_in 'username', with: user1.username
+      fill_in 'password', with: user1.password
+      click_on "Login"
+
+      visit '/users/2/edit'
+
+      fill_in 'user[username]', with: 'Larry'
+      fill_in 'user[email]', with: 'realmail@larry.com'
+      fill_in 'user[password]', with: 'wordpass'
+
+      click_on 'Update User'
+
+      expect(user2.username).to_not eq('Larry')
+      expect(user2.password).to_not eq('wordpass')
+      expect(user2.email).to_not eq('realmail@larry.com')
+    end
   end
 end
