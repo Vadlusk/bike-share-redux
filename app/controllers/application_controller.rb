745547b3773f83b909787543449cbeb08c9611a0
adam-conway <adam.n.conway@gmail.com>
2018-04-17 20:49:45 -0600

    Adding in protections for the user edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :current_admin?
   before_action :set_cart
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -13,5 +15,9 @@ class ApplicationController < ActionController::Base
 
   def set_cart
     @cart ||= Cart.new(session[:cart])
+  end
+
+  def record_not_found
+    render file: '/public/404'
   end
 end
--- spec/features/visitor/orders/visitor_cannot_access_another_users_order_spec.rb
+++ spec/features/visitor/orders/visitor_cannot_access_another_users_order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'user sees all conditions' do
-  scenario 'and all their attributes' do
+describe 'visitor goes to restricted page' do
+  scenario 'tries to go to order page' do
     order = create(:order)
     accessory1 = create(:accessory)
     accessory2 = create(:accessory)
@@ -11,10 +11,17 @@ describe 'user sees all conditions' do
     create(:order_accessory, accessory_id: accessory3.id, order_id: order.id)
 
     visit order_path(order)
-    save_and_open_page
 
     expect(page).to_not have_content(accessory1.title)
     expect(page).to_not have_content(accessory2.title)
     expect(page).to_not have_content(accessory3.title)
+  end
+  scenario 'tries to go to order page' do
+    user = create(:user)
+
+    visit edit_user_path(user)
+    expect(page).to_not have_content("Username")
+    expect(page).to_not have_content("Email")
+    expect(page).to_not have_content("Password")
   end
 end
